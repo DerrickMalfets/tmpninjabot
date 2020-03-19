74765fea288e23d422d25fea09e60b0db592b66e
DerrickMalfets <62380973+DerrickMalfets@users.noreply.github.com>
2020-03-19 18:08:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, Filters
 import requests
 
-app = Client("bot", api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e", bot_token="*")
+app = Client("bot", api_id=1227887, api_hash="33fff4db071026e1ebcb931382c50afc", bot_token="1134477113:AAELsIBFddKu6Cuahjho-cFrJdt14WXpXsQ")
 
 
 @app.on_message(Filters.command(["start"]) & Filters.private)
